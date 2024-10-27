55c9a39dcd7ec5b20bb6dcca58bd0078d5928ac0
vadimlelik <promiti90@gmail.com>
2024-10-27 16:02:03 +0300

    fix:route auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,6 +40,11 @@ export const login = async (req, res) => {
 	try {
 		const { email, password } = req.body
 
+		if (!email || !password) {
+			return res
+				.status(400)
+				.json({ message: 'Email and password are required' })
+		}
 		const user = await User.findOne({ email })
 		if (!user) {
 			return res.status(400).json({ message: 'User not found' })
