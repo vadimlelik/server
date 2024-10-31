33e0847c90cfe83a40084ff2ff2f7f871dd9ac30
vadimlelik <promiti90@gmail.com>
2024-10-31 09:15:10 +0300

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,6 @@ export const register = async (req, res) => {
 	res.cookie('refreshToken', refreshToken, {
 		maxAge: 7 * 24 * 60 * 60 * 1000,
 		httpOnly: true,
-		domain: 'localhost',
 		secure: false,
 		sameSite: 'lax',
 	})
